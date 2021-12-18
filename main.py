d395063fedd2701c36ba4a512f08fb21e621f8a0
SileboxUnderfined <valerkafm@gmail.com>
2021-12-18 17:03:36 +0300

    chaning all to kivymd

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import kivy
 kivy.require('2.0.0')
 
-from kivy.app import App
+from kivymd.app import MDApp as App
 from kivy.core.text import LabelBase
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager  import ScreenManager
 from kivy.lang import Builder
 
 from ui.mainMenu import MenuScreen
@@ -16,9 +16,10 @@ def builderLoader():
 class MyApp(App):
     def screens(self):
         self.sm.add_widget(MenuScreen(name="menu"))
-        self.sm.add_widget(GameScreen(name="game", page=1))
+        self.sm.add_widget(GameScreen(name="game", page=1, autoSaveEnabled=False))
 
     def build(self):
+        self.theme_cls.theme_style = "Dark"
         self.sm = ScreenManager()
         self.screens()
         return self.sm
@@ -26,6 +27,7 @@ class MyApp(App):
 def fontsInitializer():
     LabelBase.register(name="CubicPixel", fn_regular='ui/fonts/CubicPixel.otf')
     LabelBase.register(name="BreakPassword",fn_regular='ui/fonts/BreakPassword.otf')
+    LabelBase.register(name="CourierNew",fn_regular='ui/fonts/CourierNew.ttf',fn_bold='ui/fonts/CourierNewBold.ttf')
 
 if __name__ in "__main__":
     fontsInitializer()
--- ui/gameScreen.py
+++ ui/gameScreen.py
@@ -1,17 +1,32 @@
-import requests, bs4
+import requests, bs4, json, os
 from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
+from kivy.uix.image import AsyncImage
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
 
 class GameScreen(Screen):
-    def __init__(self, name, page):
+    def __init__(self, name, page=0, autoSaveEnabled=None):
         super(GameScreen, self).__init__()
         self.name = name
         self.page = page + 1901
         self.url = "https://www.mspaintadventures.ru/?s=6&p=00"
-        self.ids.page.text = str(self.page)
+        #self.ids.page.text = str(self.page)
+        self.loadGame(True)
+        self.autoSaveEnabled = autoSaveEnabled
+        self.ids.autoSave.text = self.checkAutoSave()
         self.loadPage()
 
     def loadPage(self):
+        self.ids.autoSave.text = self.checkAutoSave()
+        if self.page == 1901:
+            self.ids.previousPageButton.disabled = True
+        else:
+            self.ids.previousPageButton.disabled = False
+
+        if self.autoSaveEnabled:
+            self.saveGame()
+
         r = requests.get(self.url + str(self.page))
         soup = bs4.BeautifulSoup(r.text,'html.parser')
         rh = self.headerText(soup)
@@ -24,17 +39,23 @@ class GameScreen(Screen):
         return True
 
     def centerImage(self, soup):
+        self.ids.center.clear_widgets()
         tr = soup.find('tr',class_="comic-images")
-        print(tr.img['src'])
-        self.ids.center.source = tr.img['src']
+        imgs = tr.findAll('img')
+        for img in imgs:
+            self.ids.center.add_widget(AsyncImage(source=img['src'],anim_delay=0.3))
+
         return True
 
     def actionTextGet(self, soup):
+        self.ids.actionText.clear_widgets()
         td = soup.find('td', attrs={"style":"font-weight: bold; font-family: Courier New, monospace;color:#000000;line-height: 1.35"})
         ps = td.findAll('p')
+        ps.pop(0)
         for p in ps:
-            self.ids.actionText.add_widget(Label(text=p.text))
+            self.ids.actionText.add_widget(Label(text=p.text,font_name="CourierNew",bold=True, halign="justify"))
 
+        print(self.ids.actionText.children)
         return True
 
     def buttonNext(self, soup):
@@ -48,3 +69,53 @@ class GameScreen(Screen):
     def previousPage(self):
         self.page -= 1
         self.loadPage()
+
+    def autoSave(self):
+        self.autoSaveEnabled = not self.autoSaveEnabled
+        self.ids.autoSave.text = self.checkAutoSave()
+        self.saveGame()
+
+    def checkAutoSave(self):
+        if self.autoSaveEnabled == True:
+            return "Отключить автосохранение"
+
+        else:
+            return "Включить автосохранение"
+
+    def saveGame(self):
+        f = open('save.json','wt')
+        data = {"page":self.page,"autoSave":self.autoSaveEnabled}
+        json.dump(data,f)
+        f.close()
+        popup = Popup(title='Игра сохранена!',
+                      content=Label(text="Нажмите в любое место за уведомлением чтобы закрыть его"),size=(500,100),size_hint=(None,None))
+
+        popup.open()
+
+    def loadGame(self, initial=False):
+        try:
+            f = open('save.json','rt')
+        except FileNotFoundError:
+            return
+
+        data = json.load(f)
+        if initial == True:
+            if data['autoSave'] == False:
+                return
+
+        print("kek")
+        self.page = data['page']
+        self.autoSaveEnabled = data['autoSave']
+        f.close()
+        self.loadPage()
+        popup = Popup(title='Игра загружена!!',
+                      content=Label(text="Нажмите в любое место за уведомлением чтобы закрыть его"),size=(500,100),size_hint=(None,None))
+
+        popup.open()
+
+    def deleteGame(self):
+        os.remove('save.json')
+        popup = Popup(title='Игра удалена!',
+                      content=Label(text="Нажмите в любое место за уведомлением чтобы закрыть его"),size=(500,100),size_hint=(None,None))
+
+        popup.open()
